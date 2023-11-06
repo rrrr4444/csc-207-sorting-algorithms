7f7fc92c29ecb82dbb477e285de9a82590f4a99e
Reed <reedcolloton@gmail.com>
2023-11-06 10:58:33 -0600

    testing

---
+++ src/CollotonReedSortTester.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class CollotonReedSortTester {
+  public static void main(String[] args) {
+    CollotonReedSort sorter = new CollotonReedSort();
+    Integer[] arr = new Integer[]{3, 2, 10, 5, 2, 12, 3, 4};
+    sorter.sort(arr, Integer::compareTo);
+    assertArrayEquals(arr, new Integer[]{2, 2, 3, 3, 4, 5, 10, 12});
+    System.out.println(Arrays.toString(arr));
+  }
+}
